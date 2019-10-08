2431a0690ed281e4776a51bc5bf29924a02fc791
Gianluca-L <gluca1993.gl@gmail.com>
2019-10-08 23:14:54 +0200

    color click

--- sketch.js
+++ sketch.js
@@ -11,7 +11,8 @@ var h = 0;
 var r = false;
 var mr = 0;
 var y = false;
-var colorList = [(252, 98, 3),(252, 206, 3),(0,0,0)];
+
+var randomColor;
 function preload(){
   // put preload code here
 }
@@ -28,13 +29,12 @@ function setup() {
     //var tempr = random()* 50 + 10;
     var tempr = width/108/2;
     var tempBall = new Ball(tempx, tempy, tempr); // istance
-    console.log(tempBall);
+
     tempBall.color = color(random()*255, 0, random()*255);
     tempBall.speed = 3;
 
     allMyBalls.push(tempBall);
   }
-   console.log(allMyBalls);
 }
 
 function draw() {
@@ -46,16 +46,6 @@ function draw() {
 
   for(var i = 0; i < allMyBalls.length; i++) { // prendo ogni istanza e chiamo  metodo
     var tempBall = allMyBalls[i];
-    // a += 1/60;
-    // if (frameCount/60 == a*2) {
-    //   tempBall.color = lerpColor(color("white"),color("red"),frameCount/60);
-    // }
-    // else {
-    //   tempBall.color = lerpColor(color("red"),color("white"),frameCount/120);
-    // }
-    // if (m == true) {
-    //   tempBall.color = color(random()*255, random()*255, random()*255);
-    // }
 
     tempBall.move();
     tempBall.display();
@@ -63,9 +53,7 @@ function draw() {
     tempBall.clickColor();
 
   }
-  if (n == true) {
-    tempBall.color = color("green");
-  }
+
   push();
   translate(width/2, height);
   fill('white');
@@ -79,15 +67,14 @@ function draw() {
 
 
 function Ball(_x, _y,_diameter){
-  // inner properties
-  this.size = _diameter; // variabile che può essere modificata all'esterno dell'oggetto
+
+  this.size = _diameter;
   this.x = _x;
   this.y = _y;
   this.speed = 2;
   this.color = 'gold'
-  var colorHex = random(colorList);
 
-  var xIncrease = 1;  // ora sì
+  var xIncrease = 1;
   var yIncrease = 1;
 
 
@@ -129,8 +116,7 @@ function Ball(_x, _y,_diameter){
   }
   this.clickColor = function() {
     if (r == true) {
-      this.color = 'red';
-
+      this.color = randomColor;
     }
   }
 }
@@ -150,24 +136,9 @@ else if (mouseX > pmouseX){ // dx
   g = +1;
 }
 
-// else if (mouseY < pmouseY && mouseX > pmouseX) {
-//   h = -1;
-//   g = +1;
-// }
-// else if (mouseY < pmouseY && mouseX < pmouseX) { // su sx
-//   h = -1;
-//   g = -1;
-// }
-// else if (mouseY > pmouseY && mouseX > pmouseX) { // giù dx
-//   h = +1;
-//   g = +1;
-// }
-// else if (mouseY > pmouseY && mouseX < pmouseX) { // giù sx
-//   h = +1;
-//   g = -1;
-// }
 }
 function mouseReleased() {
+  randomColor = color(random()*255, random()*255, random()*255);
   mr++;
   if (mr == 1) {
     r = true;
